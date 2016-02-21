c5ff1204ac17484bbc0a0e6a9c4f8662a5622ed5
Hüseyin Koyun <prof.huko@gmail.com>
2016-02-21 10:55:38 +0100

    copyright

--- ajax.php
+++ ajax.php
@@ -2,7 +2,7 @@
 /**
  * Expenia Ziyaretçi Defteri Scripti v2.0     
  *
- * Expenia Internet Solutions tarafından ücretsiz dağıtılmakta olan bir ziyaretçi defteri scriptidir.
+ * Expenia IT-Solutions tarafından ücretsiz dağıtılmakta olan bir ziyaretçi defteri scriptidir.
  * GNU Public License ile korunmakta olup altta olan linki kaldırmak yasaktır.
  * 
  * Bu betik üzerinde değişiklik yaparak veya yapmayarak kullanabilirsiniz.
@@ -22,44 +22,46 @@
 include_once "libs/config.inc.php";
 
 // eğer XHR isteği gönderilmişse
-if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){
-	// approve message
-	if(isset($_GET['onayla'])){
-		if(isset($_SESSION['id'])){
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+	
+	// mesaj onaylama islemi
+	if (isset($_GET['onayla'])) {
+		if (isset($_SESSION['id'])) {
 			$id = intval($_POST['msg_id']);
 			
-			if($zd->db->query("UPDATE messages SET approval = '1' WHERE id = '$id'")){
+			if ($zd->db->query("UPDATE messages SET approval = '1' WHERE id = '$id'")) {
 				echo 1;
-			}else{
+			} else {
 				header("location: http://www.google.com");
 			}
 		}
 	}
 	
 	// delete message
-	if(isset($_GET['sil'])){
-		if(isset($_SESSION['id'])){
+	if (isset($_GET['sil'])) {
+		if (isset($_SESSION['id'])) {
 			$id = intval($_POST['msg_id']);
 			
-			if($zd->db->query("DELETE FROM messages WHERE id = '$id'")){
+			if ($zd->db->query("DELETE FROM messages WHERE id = '$id'")) {
 				echo 1;
 				$zd->db->query("DELETE FROM messages WHERE parent_id = '$id'");
-			}else{
+			} else {
 				header("location: http://www.google.com");
 			}
 		}
 	}
 	
 	// reply message
-	if(isset($_GET['cevapla'])){
-		if(isset($_SESSION['id'])){
+	if (isset($_GET['cevapla'])) {
+		if (isset($_SESSION['id'])) {
 			$id = intval($_POST['m_id']);
 			$response = nl2br($zd->security($_POST['response']));
-			if(!empty($response)){
-				if($zd->db->query("INSERT INTO messages (message, parent_id) VALUES ('$response', '$id')")){
+			
+			if (!empty($response)) {
+				if ($zd->db->query("INSERT INTO messages (message, parent_id) VALUES ('$response', '$id')")) {
 					echo 'OK';
 				}
-			}else{
+			} else {
 				echo 'Can\'t be empty!';
 			}
 		}
